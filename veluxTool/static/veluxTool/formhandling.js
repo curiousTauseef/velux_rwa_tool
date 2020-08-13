bd5ec04a02f52c20946c2c240617dcc79bfdb2c5
tauseef <tau.leuven@gmail.com>
2020-08-13 18:16:13 +0200

    pdf gen for 3 smoke compartments work

--- veluxTool/static/veluxTool/formhandling.js
+++ veluxTool/static/veluxTool/formhandling.js
@@ -16,6 +16,52 @@ $(document).ready(function () {
     var buildingTypeOpt = buildingType.options[buildingType.selectedIndex];
 
 
+    //var doc = new jsPDF();
+    
+
+
+    /*
+    $('#calcformnew input, #calcformnew select').each(
+
+        //doc.text(20, 20, 'Velux tool for calculating ventilation of buildings. Your results are below!');
+
+        function(index){  
+            var input = $(this);
+            //alert('Type: ' + input.attr('type') + 'Name: ' + input.attr('name') + 'Value: ' + input.val());
+
+            var newLine = "\r\n"
+            var msg = "Get the details of elements from the specific form with the form id"
+            msg += newLine;
+            msg += "Type: " + input.attr('type');
+            msg += newLine;
+            msg += "Name: "+ input.attr('name');
+            msg+= newLine;
+            msg += "Value: "+ input.val();
+            alert(msg);
+            console.log(input)
+        }
+
+        doc.save('Input.pdf')  
+    );
+
+    */
+
+    /*
+
+    var formElements = new Array();
+    var j=30;
+    $('#calcformnew input, #calcformnew select').each(function(){
+        
+        formElements.push($(this));
+        doc.text(20, j, formElements);
+        j=j+5;
+    });
+
+    doc.save('Input.pdf'); 
+
+    */
+
+
 
     $(document).on('change', '#smoke_compts', () => {
         updateSmokeCompts();
@@ -101,7 +147,75 @@ $(document).ready(function () {
     }
 
 
+    function updateSCResults() {
+        //let sc1_occ = $('#sc_occupancy_list'+usrNr).val();
+        //console.log(provincie);
+        //let brandweerzone=$("#brandweerzone");
+        //let values=[];
+        var i;
 
+        for(i = 0; i < arguments.length; i++) {
+            let sc_occ = $('#sc_occupancy_list'+arguments[i]).val();
+
+            switch(sc_occ) {
+                case 'badhuizen':
+                case 'boekerijen':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('12m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('9msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('3,0 X 3,0m');
+                    break;
+
+                case 'Accumulatorenstations':
+                case 'Beton':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('18m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('20msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('4,5 X 4,5m');
+                    break;
+    
+                default:
+                    
+            }
+
+
+
+        }
+
+
+    }
+
+    function updateSC2Results() {
+        let sc2_occ = $('#sc2_occupancy_list').val();
+        //console.log(provincie);
+        //let brandweerzone=$("#brandweerzone");
+        //let values=[];
+        switch(sc2_occ) {
+            case 'badhuizen':
+
+            case 'boekerijen':
+                $("#sc2display :input[name=Circumference]").val('3');
+
+            default:
+                
+        }
+
+    }
+
+    function updateSC3Results() {
+        let sc3_occ = $('#sc3_occupancy_list').val();
+        //console.log(provincie);
+        //let brandweerzone=$("#brandweerzone");
+        //let values=[];
+        switch(sc3_occ) {
+            case 'badhuizen':
+
+            case 'boekerijen':
+                $("#sc3display :input[name=Circumference]").val('3');
+
+            default:
+                
+        }
+
+    }
 
 
 
@@ -151,7 +265,7 @@ $(document).ready(function () {
      //source1 = $('#calcformnew')[0];
 
 
-
+/*
     $('#cmd').click(function () {
         var doc = new jsPDF();
         doc.text(20, 20, 'Velux tool for calculating ventilation of buildings. Your results are below!');
@@ -192,7 +306,86 @@ $(document).ready(function () {
 
 
 
-    });
+    }); //cmd ends
+    */
+
+
+   $('#ventCalculation').click(function (event) {
+
+        //updateSCResults();
+        updateSCResults(1,2,3);
+
+
+
+    }); //ventCalculation ends
+
+
+
+    $('#pdfSave').click(function (event) {
+        var doc = new jsPDF();
+        doc.text(20, 20, 'Velux tool for calculating ventilation of buildings. Your results are below!');
+
+        var formElements = new Array();
+        var j=30;
+        //var n=0;
+        var indSelect=0;
+
+
+        //var msg = "Get the details of elements from the specific form with the form id"
+
+        var newLine = "\r\n"
+        $('#calcformnew input, #calcformnew select').each(function(){
+        
+            var input = $(this);
+            var s = $(this);
+           
+            if( !(typeof input.attr('name') === 'undefined')  ){
+                
+                //if(input.tagName === 'INPUT' && input.type === 'text') {
+                if(input.is('input')) {
+
+                    var msg = input.attr('name') + ':   '+input.val();
+                    doc.text(20, j,  msg  );
+                    j=j+3;
+                }
+                j=j+2;
+                //if(input.tagName === 'SELECT') {
+                if(input.is('select')) {
+
+                    var userval = s[0].options[s[0].selectedIndex].text;
+                    var msg = input.attr('name') + ':   '+userval;
+    
+                    //doc.text(20, j,  msg  );
+
+                    if(   (parseInt(input.val()) != 0)  && !( input.val().trim() ===("base").valueOf() )  && !( input.val().trim() ===("").valueOf() ) ) {
+                            doc.text(20, j,  msg  );
+                            j=j+5;
+                    }
+               
+                }
+
+            }
+   
+
+
+    
+    
+            //j=j+1;
+            //n=n+1;
+        });
+
+
+    
+        formElements.forEach(element => console.log(element));
+        doc.save('Input.pdf'); 
+        event.preventDefault();
+      
+
+
+    }); //cmd ends
+
+
+
     
 
 
