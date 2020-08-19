4da17d68b1df0346f13037b3aa2cbbe109534d20
tauseef <tau.leuven@gmail.com>
2020-08-19 12:12:55 +0200

    See if JS is being referenced properly

--- veluxTool/static/veluxTool/formhandling.js
+++ veluxTool/static/veluxTool/formhandling.js
@@ -1,65 +1,12 @@
 $(document).ready(function () {
 
     //form validation JS borrowed from https://getbootstrap.com/docs/4.3/components/forms/?#how-it-works
-    console.log("Import http New DOM ready for pdf handling in veluxTool");
+    console.log("JS ready for updating HTML elements in veluxTool");
 
     //Globals after DOM is ready
     var buildingType = document.getElementById('buildingType');
     // reference to selected option
     var buildingTypeOpt = buildingType.options[buildingType.selectedIndex];
-
-    (function(API) {
-		API.textAlign = function(txt, options, x, y) {
-				options = options || {};
-				// Use the options align property to specify desired text alignment
-				// Param x will be ignored if desired text alignment is 'center'.
-				// Usage of options can easily extend the function to apply different text
-				// styles and sizes
-
-				// Get current font size
-				var fontSize = this.internal.getFontSize();
-
-				// Get page width
-				var pageWidth = this.internal.pageSize.width;
-
-				// Get the actual text's width
-				// You multiply the unit width of your string by your font size and divide
-				// by the internal scale factor. The division is necessary
-				// for the case where you use units other than 'pt' in the constructor
-				// of jsPDF.
-
-				var txtWidth = this.getStringUnitWidth(txt) * fontSize / this.internal.scaleFactor;
-
-				if (options.align === "center") {
-
-						// Calculate text's x coordinate
-						x = (pageWidth - txtWidth) / 2;
-
-				} else if (options.align === "centerAtX") { // center on X value
-
-						x = x - (txtWidth / 2);
-
-				} else if (options.align === "right") {
-
-						x = x - txtWidth;
-				}
-
-				// Draw text at x,y
-				this.text(txt, x, y);
-		};
-		/*
-		    API.textWidth = function(txt) {
-			    var fontSize = this.internal.getFontSize();
-		        return this.getStringUnitWidth(txt)*fontSize / this.internal.scaleFactor;
-		    };
-		*/
-
-		API.getLineHeight = function(txt) {
-				return this.internal.getLineHeight();
-		};
-
-    })(jsPDF.API);
-
 
 
     $(document).on('change', '#smoke_compts', () => {
@@ -68,7 +15,7 @@ $(document).ready(function () {
 
     function updateSmokeCompts() {
        let n = parseInt($('#smoke_compts').val());
-       console.log("number of smoke compartments selected");
+       console.log("# of smoke compartments selected");
        console.log(n);
        if(n==0){
            document.getElementById('smokecompt').style.display = 'none';
@@ -114,11 +61,13 @@ $(document).ready(function () {
 
     function updateSCResults() {
         var i;
+        console.log("update the sc fire characteristics");
 
         //for(i = 0; i < arguments.length; i++) {
         for(i = 0; i < 1; i++) {
             let sc_occ = $('#sc_occupancy_list'+arguments[i]).val();
-
+            console.log(sc_occ);
+            console.log(arguments[i]);
             switch(sc_occ) {
                 //Klein risico with Categorie 1 (L)
                 case 'Badhuizen':
@@ -144,6 +93,52 @@ $(document).ready(function () {
                     $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('20msq');
                     $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('4,5 X 4,5m');
                     break;
+                //Normal risico with Categorie 3 (N3)
+                case 'Bakelietfabrieken':
+                case 'Banden voor brandopnemers en dgl.':
+                case 'Banketbakerijnen':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('24m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('36msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('6,0 X 6,0m');
+                    break;
+                //Normal risico with Categorie 4 (N4)
+                case 'Afval en lompen(textiel)':
+                case 'Alcohol(stokerij, magazijnen, aftrekkerijnen van':
+                case 'Azijnfabrieken':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('36m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('81msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('9,0 X 9,0m');
+                    break;
+                //High risico with Categorie 4 (D1)
+                case 'Confectiewerkplaatsen':
+                case 'Houtverkoling':
+                case 'Hout(creosoteren)':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('36m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('81msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('9,0 X 9,0m');
+                    break;
+                //High risico with Categorie 4 (D2)
+                case 'Vliegtuigloodsen':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('36m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('81msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('9,0 X 9,0m');
+                    break;
+                //High risico with Categorie 4 (D3)
+                case 'Afvalvezels(bewerken van)':
+                case 'Asfalt':
+                case 'Bandenindustrie':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('36m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('81msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('9,0 X 9,0m');
+                    break;
+                //High risico with Categorie 4 (D4)
+                case 'Acetyleen':
+                case 'Celluloid':
+                case 'Gassen in vloeibare toestand':
+                    $("#sc"+arguments[i]+"display :input[name=Circumference]").val('36m');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Area]").val('81msq');
+                    $("#sc"+arguments[i]+"display :input[name=Fire-Size]").val('9,0 X 9,0m');
+                    break;
                 default:
                   
             }
@@ -160,72 +155,25 @@ $(document).ready(function () {
     //On selection of occupancy for any of the smoke compartments, results of categories are to be displayed.
     //Rather than button click, user selection changes the results.
     $(document).on('change', '#sc_occupancy_list1', () => {
+        console.log("chng sc1 selection");
         updateSCResults(1);
     });
 
     $(document).on('change', '#sc_occupancy_list2', () => {
+        console.log("chng sc2 selection");
+
         updateSCResults(2);
     });
 
     $(document).on('change', '#sc_occupancy_list3', () => {
+        console.log("chng sc3 selection");
+
         updateSCResults(3);
     });
 
 
 
-    $('#pdfSave').click(function (event) {
-        var doc = new jsPDF();
-        //doc.text(20, 20, 'Velux tool for calculating ventilation of buildings. Your results are below!');
-        var j=30;
-        var indSelect=0;
 
-        var newLine = "\r\n";
-
-        var veluxtool_title = "Velux Tool";
-
-        doc.textAlign(veluxtool_title, {
-            align: "center"
-        }, 0, 15);
-
-        $('#calcformnew input, #calcformnew select').each(function(){
-        
-            var input = $(this);
-            var s = $(this);
-           
-           // if( !(typeof input.attr('name') === 'undefined')  ){
-                
-                //if(input.tagName === 'INPUT' && input.type === 'text') {
-                if(input.is('input') && !( input.val().trim() ===("").valueOf() )  ) {
-
-                    var msg = input.attr('name') + ':   '+input.val();
-                    doc.text(20, j,  msg  );
-                    j=j+3;
-                }
-                j=j+2;
-
-                if(input.is('select')) {
-
-                    var userval = s[0].options[s[0].selectedIndex].text;
-                    var msg = input.attr('name') + ':   '+userval;
-    
-                    //doc.text(20, j,  msg  );
-
-                    if(   (parseInt(input.val()) != 0)  && !( input.val().trim() ===("base").valueOf() )  && !( input.val().trim() ===("").valueOf() ) ) {
-                            doc.text(20, j,  msg  );
-                            j=j+5;
-                    }
-             
-                }
-           // }
-  
-            //j=j+1;
-            //n=n+1;
-        });
-
-        doc.save('Input.pdf'); 
-        event.preventDefault();
-
-    }); //cmd ends
 
         
 }); //document ready ends
